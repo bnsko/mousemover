05a38d416755563e363d28a294cd17ef7628181a
bnsko <35601638+bnsko@users.noreply.github.com>
2021-05-16 23:11:06 +0200

    Update mousemover.py

--- mousemover.py
+++ mousemover.py
@@ -22,8 +22,7 @@ def cond():
             savedpos = curpos
             print("Cursor is on this position for" ,(count+1)/2, " seconds.")
             count += 1
-            if count >= 20:  # if count is greater than 60 it means timeout will occur after mouse is steady for more than
-                # 240 seconds i.e. 4 minutes. (approx.)
+            if count >= 300:
                 print("You are AFK for 5 minutes, moving cursor for few pixels")
                 actions()
                 break

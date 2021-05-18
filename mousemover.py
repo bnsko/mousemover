719a761566934cccc8c4c0ec20955ac1cc5a1714
bnsko <35601638+bnsko@users.noreply.github.com>
2021-05-18 15:55:58 +0200

    Update mousemover.py

--- mousemover.py
+++ mousemover.py
@@ -1,26 +1,29 @@
-import pyautogui as gui
+import pyautogui as cursor
 import win32api
 from time import sleep
-savedpos = win32api.GetCursorPos()
+save_pos = win32api.GetCursorPos()
 
 def eksn():
     while True:
-        gui.move(2, 2)
-        sleep(0.2)
-        gui.move(-2, -2)
-        nomove()
+        x, y = win32api.GetCursorPos()
+        cursor.moveTo(1, 1)
+        sleep(0.1)
+        cursor.click()
+        sleep(0.1)
+        win32api.SetCursorPos((x, y))
+        donotmove()
 
-def nomove():
+def donotmove():
     count = 0
     while True:
-        savedpos = win32api.GetCursorPos()
+        save_pos = win32api.GetCursorPos()
         sleep(1)
-        curpos = win32api.GetCursorPos()
-        if savedpos == curpos:
-            savedpos = curpos
+        current_pos = win32api.GetCursorPos()
+        if save_pos == current_pos:
+            save_pos = current_pos
             print("You did not move the cursor for" ,(count+1), " seconds.")
             count += 1
-            if count >= 240: 
+            if count >= 5: 
                 print("You are AFK for 4 minutes, moving cursor for few pixels.")
                 eksn()
                 break
@@ -30,4 +33,4 @@ def nomove():
             print("Mouse is currently moving")
             count = 0
 
-nomove()
+donotmove()
